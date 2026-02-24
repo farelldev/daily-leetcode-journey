84f5e8e73dc9d9e08997c7266d8ff6a550ccf53f
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-02-24 08:48:58 +0700

    Time: 0 ms (100%), Space: 19.5 MB (24.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        order = []
+        stack = []
         pair = {'}' : '{',
                 ')' : '(',
                 ']' : '['}
         for i in range(len(s)):
             if s[i] in ['(', '{', '[']:
-                order.append(s[i])
+                stack.append(s[i])
             else:
-                if order and order[len(order)-1] == pair[s[i]]:
-                    order.pop(len(order) - 1)
+                if stack and stack[len(stack)-1] == pair[s[i]]:
+                    stack.pop()
                 else: return False
 
-        return not order
+        return not stack
