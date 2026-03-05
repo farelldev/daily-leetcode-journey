2c35d1a12a81eda760236d51eddb825120186086
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-03-05 21:53:03 +0700

    Time: 0 ms (100%), Space: 21.2 MB (93.43%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,15 +3,19 @@ class Solution:
         res = []
 
         for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            else:
+            if newInterval[1] < intervals[i][0]: # If newIn behind interval
+                res.append(newInterval) # Append the newIn
+                return res + intervals[i:]  # Return it right away
+
+            elif newInterval[0] > intervals[i][1]: # If interval behind newIn 
+                res.append(intervals[i])    # Append that interval
+
+            else: # If overlapping
+                # Extend newInterval
                 newInterval = [min(newInterval[0], intervals[i][0]),
                     max(newInterval[1], intervals[i][1])]
 
+        # Append newInterval at the end
         res.append(newInterval)
 
         return res
