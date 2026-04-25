5fc3e489c598a8f8f7e843efe085786e5940144e
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-04-25 21:55:49 +0700

    Time: 11 ms (76.18%), Space: 19.5 MB (29.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return "".join(sorted(s)) == "".join(sorted(t))
+        letter = {}
+
+        for i in s:
+            if i not in letter:
+                letter[i] = 1
+            else:
+                letter[i] += 1
+
+        for i in t:
+            if i not in letter or letter[i] == 0: 
+                return False
+            else:
+                letter[i] -= 1
+
+        return sum(letter.values()) == 0
