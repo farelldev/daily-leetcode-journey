6b3e20b66076ad28d5ca987c328c97e01dd4ecd4
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-07-22 21:34:45 +0700

    Time: 0 ms (100%), Space: 19.3 MB (92.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
-
-        def create_subset(i):
+        def createSubset(i, subset):
             if i == len(nums):
-                res.append(subset[:])
+                res.append(copy.copy(subset))
                 return
-            
+
             subset.append(nums[i])
-            create_subset(i+1)
-
+            createSubset(i + 1, subset)
             subset.pop()
-            create_subset(i+1)
+            createSubset(i + 1, subset)
 
-        create_subset(0)
+        res = []
+        createSubset(0, [])
         return res
