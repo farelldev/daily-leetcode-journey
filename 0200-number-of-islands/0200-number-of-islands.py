d1d4b3b88a5c4a3a540275d512ddcf37f7147478
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-07-31 19:00:16 +0700

    Time: 263 ms (24.03%), Space: 22 MB (32.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,22 +6,20 @@ class Solution:
         seen = [[False for _ in range(col)] for _ in range(row)]
         res = 0
 
-        def bfs(x, y):
+        def dfs(x, y):
             if x == len(grid) or y == len(grid[0]) or x == -1 or y == -1 or seen[x][y] == True or grid[x][y] == '0':
                 return
 
             seen[x][y] = True
-            bfs(x, y + 1)
-            bfs(x + 1, y)
-            bfs(x, y - 1)
-            bfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x + 1, y)
+            dfs(x, y - 1)
+            dfs(x - 1, y)
 
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == '1' and seen[i][j] == False:
-                    bfs(i, j)
+                    dfs(i, j)
                     res += 1
-                    print(i, j)
-
-        print(seen)
+                    
         return res
