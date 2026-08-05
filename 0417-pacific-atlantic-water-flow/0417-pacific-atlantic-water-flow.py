24d1910247106455ee5f34b7f8c0a9d5bd287469
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-08-05 12:54:59 +0700

    Time: 31 ms (55.98%), Space: 22.4 MB (25.19%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,21 +1,35 @@
 class Solution:
-    def pacificAtlantic(self, heights):
-        if not heights:
-            return []
-        m, n = len(heights), len(heights[0])
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        
-        def dfs(i, j, visited):
-            visited.add((i, j))
-            for dx, dy in directions:
-                x, y = i + dx, j + dy
-                if 0 <= x < m and 0 <= y < n:
-                    if (x, y) not in visited and heights[x][y] >= heights[i][j]:
-                        dfs(x, y, visited)
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        row = len(heights)
+        col = len(heights[0])
+        dirs = [(1,0),(0,1),(-1,0),(0,-1)]
 
-        pacific, atlantic = set(), set()
-        for j in range(n): dfs(0, j, pacific)
-        for i in range(m): dfs(i, 0, pacific)
-        for j in range(n): dfs(m-1, j, atlantic)
-        for i in range(m): dfs(i, n-1, atlantic)
-        return list(pacific & atlantic)
+        canPac = [[False for _ in range(col)] for _ in range(row)]
+        canAtl = [[False for _ in range(col)] for _ in range(row)]
+        res = []
+        
+        def dfs(i, j, ocean):
+            if 0 <= i < row and 0 <= j < col and not ocean[i][j]:
+                ocean[i][j] = True
+
+                for dx, dy in dirs:
+                    adjX = i + dx
+                    adjY = j + dy
+                    if 0 <= adjX < row and 0 <= adjY < col and heights[i][j] <= heights[adjX][adjY]:
+                        dfs(i + dx, j + dy, ocean)
+
+        for i in range(row):
+            dfs(i, 0, canPac)
+            dfs(i, col - 1, canAtl)
+
+        for j in range(col):
+            dfs(0, j, canPac)
+            dfs(row - 1, j, canAtl)
+
+        for i in range(row):
+            for j in range(col):
+                if canPac[i][j] and canAtl[i][j]:
+                    res.append([i, j])
+
+        return res
+        
