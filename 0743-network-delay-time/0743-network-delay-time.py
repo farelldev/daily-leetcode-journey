76cad9dafd7fe950c38e986480ada2d94adfd4ad
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-08-18 23:01:41 +0700

    Time: 355 ms (21.46%), Space: 21.7 MB (14.76%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,12 +1,26 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        dist = [float('inf')] * (n + 1)
-        dist[k] = 0
-        
-        for _ in range(n - 1):
-            for u, v, w in times:
-                if dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-                    
-        res = max(dist[1:])
-        return res if res < float('inf') else -1
+        import heapq
+        from collections import defaultdict
+
+        frontier = [(0, k)]
+        visit = set()
+        neig = defaultdict(list)
+        res = 0
+
+        for time in times:
+            neig[time[0]].append((time[1], time[2]))
+
+        while frontier:
+            curr = heapq.heappop(frontier)
+            if curr[1] in visit: continue
+            
+            visit.add(curr[1])
+            res = curr[0]
+
+            for i in neig[curr[1]]:
+                heapq.heappush(frontier, (curr[0] + i[1], i[0]))
+
+        if len(visit) != n: res = -1
+        print(visit)
+        return res
