8616b26d1f251628066c6e4d0885cd3415547c1d
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-09-20 15:52:56 +0700

    Time: 55 ms (82.26%), Space: 22.5 MB (25.19%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,34 +1,29 @@
 class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
+    def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
+        if endWord not in wordList: return 0
 
-        nei = collections.defaultdict(list)
-        wordList.append(beginWord)
-        for word in wordList:
-            for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j + 1:]
-                nei[pattern].append(word)
-
-        visit = set([beginWord])
+        neighbor = collections.defaultdict(list)
+        if beginWord not in wordList: wordList.append(beginWord)
+        visited = {beginWord}
         q = deque([beginWord])
         res = 1
+
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                neighbor[pattern].append(word)
 
         while q:
             for i in range(len(q)):
                 word = q.popleft()
-
-                if word == endWord:
-                    return res
+                if word == endWord: return res
 
                 for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j + 1 :]
-
-                    for neiWord in nei[pattern]:
-                        if neiWord not in visit:
-                            visit.add(neiWord)
-                            q.append(neiWord)
-
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for neiWord in neighbor[pattern]:
+                        if neiWord == word or (neiWord in visited): continue
+                        q.append(neiWord)
+                        visited.add(neiWord)
             res += 1
 
         return 0
